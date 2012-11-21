408baeb59f8e11a059ff4e23251f21b8256d56aa
Brightfish Cinemapub <support@cinemapub.be>
2012-11-21 16:17:05 +0100

    Copied from original sourceforge site
    migrating the project ...

---
+++ waNOCAPS/readme.md
@@ -0,0 +1,44 @@
+====== waNOCAPS =========
+ CAPSLOCK  Terminator 2.0
+=========================
+
+Does this sound familiar: 
+* You've never used the CAPS LOCK key on purpose, you've never needed it. 
+* You've accidently pushed the key at least a thousand times. 
+* YOU CONTINUE TYPING FOR ABOUT 10-30 SECONDS BEFORE YOU REALISE IT'S ON. 
+* You have to retype the whole sentence 
+  (You don't have a convert-to-lowercase macro). 
+* You have to hit the CAPSLOCK and SHIFT key about 5 times to figure out 
+  how to turn it off on this particular keyboard. 
+
+For you, I've created waNOCAPS, the CAPSLOCK Terminator. 
+It's not a program, it's a registry adjustment ('hack'). 
+Inspiration came from: http://www.nthelp.com/50/remapctl.htm 
+It has been tested on Win2000 and WinXP.
+
+USAGE
+-----
+DISABLE YOUR CAPSLOCK: 
+  * double-click disable_caps.reg 
+  * reboot 
+  * (Your CAPSLOCK actually becomes a 3rd CTRL-key) 
+RE-ENABLE YOUR CAPSLOCK: 
+  * double-click enable_caps.reg 
+  * reboot 
+
+
+HISTORY
+-------
+2003-11-03 - v1.0 - first public release
+2004-06-15 - v2.0 - fixed occasional freeze on reboot after enable_caps
+
++======================================================================+
+| (c) 2004 WinAdminUtils -  http://winadmin.sourceforge.net by pforret |
+| Open Software License  -  http://opensource.org/licenses/osl-1.1.txt |
+| The Original Work is provided under this License on an "AS IS" BASIS |
+| and WITHOUT WARRANTY,  either express or implied, including, without |
+| limitation,  the warranties of NON-INFRINGEMENT,  MERCHANTABILITY or |
+| FITNESS FOR A PARTICULAR PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY |
+| OF THE ORIGINAL WORK IS WITH YOU.                                    |
++======================================================================+
+
---
+++ waTimer/waTimeThis.cmd
@@ -0,0 +1,59 @@
+@echo off
+setlocal
+set PROG_NAME=%0
+set PROG_VERS=v1.0
+set PROG_AUTH=pforret@users.sourceforge.net
+
+:: ===== BASIC TESTS
+if "%1" == ""     goto usage
+if "%1" == "/?"   goto usage
+if "%1" == "/ver" goto license
+set INVOKER=call 
+if /I "%1" == "/I" set INVOKER=start /WAIT
+
+:: ===== START REAL PROGRAM
+:execute
+echo ____  START @ [%DATE% %TIME%] ____
+
+set ID=%PROG_NAME%
+waTimer START %ID%
+
+%INVOKER% %*
+
+waTimer STOP %ID% /calc /scale:hr
+
+echo ____ FINISH @ [%DATE% %TIME%] ____
+endlocal
+
+goto :end
+:: ########## START OF GENERIC PROCEDURES ##########
+:usage
+	echo _____  %PROG_NAME% %PROG_VERS% - by %PROG_AUTH%  _____
+	echo.
+	echo Usage: %PROG_NAME% [/ver] [/I] COMMAND [PARAM1] [PARAM2] ...
+	echo.
+	echo Options :
+	echo /ver    : version - show version and license info
+	echo /I      : COMMAND is executed in popup CMD window (e.g. for programs with a lot of output)
+	echo COMMAND : command that has to be timed
+	echo PARAM1/2: parameters for this command
+	echo.
+	echo Example: %PROG_NAME% xcopy d:\folder1 d:\folder2
+	echo          %PROG_NAME% ping www.sun.com
+goto :end
+
+:license
+	echo "_____   %PROG_NAME% %PROG_VERS% - by %PROG_AUTH%   _____"
+	echo +======================================================================+
+	echo + (c) 2003 WinAdminUtils -  http://winadmin.sourceforge.net by pforret +
+	echo + Open Software License  -  http://opensource.org/licenses/osl-1.1.txt +
+	echo + The Original Work is provided under this License on an 'AS IS' BASIS +
+	echo + and WITHOUT WARRANTY,  either express or implied, including, without +
+	echo + limitation,  the warranties of NON-INFRINGEMENT,  MERCHANTABILITY or +
+	echo + FITNESS FOR A PARTICULAR PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY +
+	echo + OF THE ORIGINAL WORK IS WITH YOU.                                    +
+	echo +======================================================================+
+goto :end
+
+::------------------------------------------------
+:end
